a987b0ead57271341212f21259dffa841fcd1d94
Yaresko Vlad <yareskofamily@gmail.com>
2022-12-27 23:32:17 +0200

    Опять всё поломал , но продолжаю бороться

--- main.js
+++ main.js
@@ -35,13 +35,13 @@ function gotoNext() {
 function pause() {
   isPlaying = false;
   clearInterval(timerID);
-  pauseBtn.innerHTML = '<i class="fa-solid fa-play"></i>';
+  pauseBtn.innerHTML = '<i class="fa fa-play"</i>';
 }
 
 function play() {
   isPlaying = true;
   timerID = setInterval(gotoNext, 4000);
-  pauseBtn.innerHTML = '<i class="fa-solid fa-pause"></i>';
+  pauseBtn.innerHTML = '<i class="fa fa-pause"></i>';
 }
 
 function pausePlay() {
